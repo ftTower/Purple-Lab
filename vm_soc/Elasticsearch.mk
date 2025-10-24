d650272ef393438334258732738d1b7fce0f7eae
ftTower <thomas.auer67@gmail.com>
2025-10-24 20:36:04 +0200

    Enhance Elasticsearch.mk with color-coded echo statements for better visibility

--- vm_soc/Elasticsearch.mk
+++ vm_soc/Elasticsearch.mk
@@ -1,4 +1,10 @@
 .SILENT:
+# Color codes for echo statements
+GREEN = \033[32m
+RED = \033[31m
+YELLOW = \033[33m
+BLUE = \033[34m
+NC = \033[0m # No Color
 
 update:
 	sudo apt update && sudo apt upgrade -y
@@ -10,11 +16,11 @@ elastic :
 	curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 	sudo apt update && sudo apt install elasticsearch
-	echo "Successfully installed elasticsearch"
+	echo "$(GREEN)Successfully installed elasticsearch$(NC)"
 	sudo cp ./conf_elastic_s.yml /etc/elasticsearch/elasticsearch.yml
 	sudo systemctl start elasticsearch
 	sudo systemctl enable elasticsearch
-	echo "Successfully configured elasticsearch"
+	echo "$(GREEN)Successfully configured elasticsearch$(NC)"
 	curl -X GET "localhost:9200"
 
 all : update dep elastic
